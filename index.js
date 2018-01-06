21663dc17fc1170790af96ba22bc949eca8f8a6a
Jay Chen <jscorpionse@yahoo.com>
2018-01-06 22:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function happyHolidaysTo(name){
 return "Happy holidays, you!"
 
 }
+
+function happyHolidaysTo(holiday, name){
+  return "Happy Independence Day, you!"
+}
+
+functioin holidayCountdown(Holiday, days){
+  return "It's 20 days until Mother days!"
+}
