4a58d513a7d0f958a9f2c966145869b7fdc0fc84
Jay Chen <jscorpionse@yahoo.com>
2018-01-06 22:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return "Happy Independence Day, you!"
 }
 
-function holidayCountdown(Holiday, days){
+function holidayCountdown(holiday, days){
   return "It\'s 20 days until Mother\'s Days!"
 }
